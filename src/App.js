a62de531c8763f012b7cf34a6b862f05e95a3861
Yatin <yatindavra@gmail.com>
2022-07-21 17:17:32 +0530

    all changes saved

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 // import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import About from "./Components/About";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+  const [mode, setMode] = useState("light");
   return (
-   <div className="blank">
-  <Navbar title="Text Utilities"/>
-  <div className="container">
-  <TextForm heading="Enter The Text to Analyze"/>
+    <>
+      <Router>
+        <Navbar title="Text Utilities" mode={mode} toggleMode={toggleMode} />
+        <div className="container">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
 
-  </div>
-   </div>
+            <Route
+            exact path="/"
+              element={<TextForm heading="Enter The Text to Analyze" />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
-
 export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+
+export default function About() {
+const [myStyle, setmyStyle] = useState(
+ {
+    color:"black",
+    backgroundColor:"white"
+})
+const [btnText, setbtnText] = useState("Enable Dark Mode")
+
+const toggleStyle=()=>{
+    if (myStyle.color === "black"){
+        setmyStyle({
+        color:"white",
+        backgroundColor:"grey"
+    })
+setbtnText("Enable Light Mode")}
+    else{
+        setmyStyle( {
+        color:"black",
+        backgroundColor:"white"
+    })
+    setbtnText("Enable Dark Mode")}
+}
+
+  return (
+    <div className="container" style={myStyle}>
+        <div className="accordion accordion-flush" id="accordionFlushExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="flush-headingOne">
+      <button className="accordion-button collapsed" type="button"  style={myStyle} data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the first item's accordion body.</div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="flush-headingTwo">
+      <button className="accordion-button collapsed" type="button"  style={myStyle} data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="flush-headingThree">
+      <button className="accordion-button collapsed" type="button"  style={myStyle} data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+    </div>
+  </div>
+<button type="button" onClick={toggleStyle} className="btn btn-primary my-3">{btnText}</button>
+</div>
+</div>
+  )
+}
